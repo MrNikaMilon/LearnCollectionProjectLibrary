64b18fb410b7e1902f5f4be311130357281d9f38
MrNikaMilon <sergnikonpav@gmail.com>
2024-10-03 16:19:23 +0300

    Something do)

--- src/main/java/com/library/entity/User.java
+++ src/main/java/com/library/entity/User.java
@@ -8,4 +8,9 @@ public class User {
     private String userName;
     private int userAge;
     private Long userId;
+
+    @Override
+    public String toString() {
+        return "Book name: " + this.userName;
+    }
 }
